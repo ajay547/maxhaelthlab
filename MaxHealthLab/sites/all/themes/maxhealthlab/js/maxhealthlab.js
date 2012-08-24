e928798c3f0e7b2fe50200fd93bc065137dce9d0
ajay <Ajay Singh@Ajay_Singh.(none)>
2012-08-24 15:41:03 +0530

    claning directory

--- MaxHealthLab/sites/all/themes/maxhealthlab/js/maxhealthlab.js
+++
@@ -1,5 +0,0 @@
- $(document).ready(function() {
-	 $('#bottomNavClose').click(function(){
-		$('div#lightbox').removeClass('lightmodal-login');
-	 });
- });
--- MaxHealthLab/sites/all/themes/maxhealthlab/templates/page-node-18.tpl.php
+++
@@ -1,6 +0,0 @@
-  <div id="content" class="column"><div class="section">
-        <div id="content-area">
-          <?php print $content; ?>
-        </div>
-
-      </div></div><!-- /.section, /#content -->
